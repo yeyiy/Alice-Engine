238858cc49e7770a4508616162ac1c8ef108cd0f
Mikoris <119640436+xiaomi2023@users.noreply.github.com>
2025-02-08 18:36:05 +0800

    Update script_parse.py
    ·新增功能：在游戏脚本中添加注释，详见README
    ·修复了在脚本中输入“&”会造成编译错误的BUG

--- script_parse.py
+++ script_parse.py
@@ -26,7 +26,7 @@ def parse_string(string: str):
     """输出字符串进行处理"""
     # 处理转义字符
     string = (string.replace("$n", "\n").replace("$s", " ")
-                    .replace("$p", "#")).replace("$lb", "{").replace("$rb", "}")
+              .replace("$p", "#")).replace("$lb", "{").replace("$rb", "}")
 
     # 删除结尾冗余换行符和空格
     while string[-1] in {"\n", " "}:
@@ -39,11 +39,24 @@ def read_script(file):
     with open(file, encoding="utf-8") as f:
         string = f.read()
 
-    return string.split("&")[0], parse_script(string.split("&")[1])  # 解析脚本文件中定义的游戏名和游戏脚本（用“&”分隔）
+    return string.split("&")[0], parse_script("".join(string.split("&")[1:]))  # 解析脚本文件中定义的游戏名和游戏脚本（用“&”分隔）
 
 
 def parse_script(string: str):
     """将脚本文件解析为Point对象"""
+
+    # 删除注释
+    comment_start = None  # 注释开始位置
+
+    for i in range(string.count("/*")):
+        for e in range(len(string)):
+            if string[e:e + 2] == "/*" and comment_start is None:
+                comment_start = e  # 标记注释开始
+
+            elif string[e:e + 2] == "*/":
+                string = string[:comment_start] + string[e + 2:]  # 删除注释
+                comment_start = None  # 初始化注释开始位置
+                break
 
     # 删除冗余空格
     after_newline = False
@@ -52,13 +65,14 @@ def parse_script(string: str):
         for i in range(len(string)):
             try:
                 if string[i] == "\n":
-                    after_newline = True    # 位于换行符之后
+                    after_newline = True  # 位于换行符之后
 
                 elif string[i] != " ":
-                    after_newline = False   # 遇到非空格字符，停止处理冗余空格
+                    after_newline = False  # 遇到非空格字符，停止处理冗余空格
 
                 if after_newline and string[i] == " ":
                     string = string[:i] + string[i + 1:]  # 当处于换行符后且遇到空格时，删除此空格
+                    after_newline = False
                     break
 
             except IndexError:
@@ -93,38 +107,39 @@ def parse_script(string: str):
     def parse_choice(choice):
         """解析选项"""
         choice = choice[1:]  # 删除选项脚本开头的“#”
-        p = None
-        q = None
+        point_start = None  # 选项对应的节点（被大括号括起）的开始位置
+        point_end = None  # 选项对应的节点（被大括号括起）的结束位置
 
         for c in range(len(choice)):
-            if choice[c] == "{" and p is None:
-                p = c  # 选项对应的节点（被大括号括起）的开始位置
+            if choice[c] == "{" and point_start is None:
+                point_start = c
 
             elif choice[c] == "}":
-                q = c  # 选项对应的节点（被大括号括起）的结束位置
+                point_end = c
 
         if "{" in choice:
-            return choice.split("{")[0], parse_point(choice[p:q + 1])  # 分别返回选项文本和选项文本对应的节点
+            return choice.split("{")[0], parse_point(choice[point_start:point_end + 1])  # 分别返回选项文本和选项文本对应的节点
 
         else:
             return choice, None  # 只返回选项文本
 
     def parse_point(point):
         """解析节点"""
-        begin = None
-        end = 0
+        point_start = None  # 节点（被大括号括起）的开始位置
+        point_end = 0  # 节点（被大括号括起）的结束位置
 
         for k in range(len(point)):
-            if point[k] == "{" and begin is None:
-                begin = k  # 节点（被大括号括起）的开始位置
+            if point[k] == "{" and point_start is None:
+                point_start = k
 
             elif point[k] == "}":
-                end = k  # 节点（被大括号括起）的结束位置
+                point_end = k
 
-        point = point[begin + 1:end]    # 截去大括号之外的冗余字符串
-        string_list = _split(point, "#")    # 通过“#”分割节点文本和选项
+        point = point[point_start + 1:point_end]  # 截去大括号之外的冗余字符串
+        string_list = _split(point, "#")  # 通过“#”分割节点文本和选项
 
         return Point(string_list[0],
-                     [parse_choice("#" + string_list[a]) for a in range(len(string_list)) if a != 0])   # 返回Point节点，参数包括节点文本和选项列表
+                     [parse_choice("#" + string_list[a]) for a in range(len(string_list)) if
+                      a != 0])  # 返回Point节点，参数包括节点文本和选项列表
 
     return parse_point(string)  # 返回对脚本的解析
