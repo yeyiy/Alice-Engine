a3cd758899917bf3f50e72cf94975fdadc4b60ac
Mikoris <119640436+xiaomi2023@users.noreply.github.com>
2025-02-10 10:44:03 +0800

    Update main.py
    ·修复了脚本文件被重复解析的问题

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ if __name__ == "__main__":
 
     # 初始化全局应用实例
     game = mainloop.GUI()  # 实例化GUI对象
-    game.window_title(script_parse.read_script(file)[0])  # 设置窗口标题
-    points = script_parse.read_script(file)[1]  # 读取脚本文件
+    script = script_parse.read_script(file)  # 读取脚本文件
+    game.root.title(script[0])  # 设置窗口标题
+    points = script[1]  # 读取脚本文件
 
-    try:
-        while True:
+    while True:
+        try:
             game.output(points.text, [i[0] for i in points.choice])  # 输出节点文本，读取并输出选项文本
-            points = script_parse.choose(points, game.input())  # 选择选项并更新节点
 
-        # noinspection PyUnreachableCode
-        game.root.mainloop()
+        except AttributeError as e:
+            raise Exception(f"由于游戏结束或遇到错误，游戏程序已停止，详细的信息如下：\n{e}")
 
-    except AttributeError as e:
-        game.output(f"由于游戏结束或遇到了错误，游戏程序已经停止，详细的输出如下：\n{e}", ["确定"])
+        points = script_parse.choose(points, game.input())  # 选择选项并更新节点
+
+    # noinspection PyUnreachableCode
+    game.root.mainloop()
